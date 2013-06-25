89d38b3e8d8dae23fe8b8b386c85fd00233f4e8b
begert <begi@gmx.ch>
2013-06-25 22:14:41 +0200

    favicon better

--- src/header.inc.php
+++ src/header.inc.php
@@ -2,8 +2,8 @@
 <html>
 <head>
   <title>fil-plus</title>
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
   <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
   <style type="text/css">
