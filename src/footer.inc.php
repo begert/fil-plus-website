3670835b9315bd8aca90009a2762ed31dcdbb19f
begert <begi@gmx.ch>
2013-07-01 11:35:07 +0200

    opening times

--- src/footer.inc.php
+++ src/footer.inc.php
@@ -11,8 +11,13 @@
             <br>
             034 445 15 73
             <br><br>
-            Mo-Fr 8 - 17<br>
-            Sa 9 - 12
+            Montag<br> 
+            <strong>geschlossen</strong><br>
+            Di-Fr<br>
+            <strong>08:30 - 12:00</strong><br>
+            <strong>13:30 - 18:00</strong><br>
+            Sa<br>
+            <strong>8:30 - 15:00</strong>
           </p>
         </div>
       </div>
