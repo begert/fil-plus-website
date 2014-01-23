57302f780a99dfbcd3cac69c170f594c6a40a5d4
begert <begi@gmx.ch>
2014-01-23 19:15:49 +0100

    navigation for admin

--- src/admin/header.inc.php
+++ src/admin/header.inc.php
@@ -14,12 +14,32 @@ $php_self = $_SERVER['PHP_SELF'];
   <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
   <style type="text/css">
       body {
+        padding-top: 50px;
         font-size: 170%;
       }
   </style>
   <script src="../js/jquery.min.js"></script>
 </head>
 <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">fil-plus admin</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="aktuell_list.php">Aktuell</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 
   <div class="container">
       
